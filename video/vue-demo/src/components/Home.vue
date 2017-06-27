759d6e31020dd512660a740e21aa0edf55527110
薄涛 <botaoxyz@163.com>
2017-06-27 22:23:03 +0800

    脚手架

---
+++ video/vue-demo/src/components/Home.vue
@@ -0,0 +1,6 @@
+<template id="home">
+    <div>
+        <h3>我是主页</h3>
+        <p>我是主页的内容</p>
+    </div>
+</template>
---
+++ video/vue-demo/src/components/News.vue
@@ -0,0 +1,21 @@
+<template id="news">
+    <div>
+        <h3>我是新闻</h3>
+        <p>我是新闻的内容</p>
+        <ul>
+            <li v-for="item in news">{{item}}</li>
+        </ul>
+    </div>
+</template>
+<script>
+    export default {
+        data () {
+            return {
+                news:["新闻一","新闻二","新闻三"]
+            }
+        },
+        components: {}
+    }
+</script>
+<style>
+</style>
